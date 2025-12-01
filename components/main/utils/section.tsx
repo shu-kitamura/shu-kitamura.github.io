143cedd3b4c3d245191baa549ca586ecf3c3cc4f
shu-kitamura <shusei3316@yahoo.co.jp>
2025-12-01 21:05:10 +0900

    add border

--- components/main/utils/section.tsx
+++ components/main/utils/section.tsx
@@ -2,8 +2,10 @@ import { JSX } from "react";
 
 export default function Section({ sectionId, contents }: { sectionId: string, contents: JSX.Element }) {
   return (
-    <section className="w-full sm:w-1/2" id={sectionId}>
-      {contents}
+    <section className="w-full sm:w-1/2 pt-1 bg-gradient-to-r from-orange-500 dark:from-orange-600 from-0% to-blue-500 dark:to-blue-800 to-100%" id={sectionId}>
+      <div className="bg-white dark:bg-gray-800 shadow-md p-4 space-y-4">
+        {contents}
+      </div>
     </section>
   );
 }
