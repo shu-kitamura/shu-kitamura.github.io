287c40799e8f3916738c6ad2b9f148b802655887
shu-kitamura <shusei3316@yahoo.co.jp>
2025-12-09 21:19:24 +0900

    remove unused dependencies

--- app/components/utils/title.tsx
+++
@@ -1,9 +0,0 @@
-export default function Title({ title }: { title: string }) {
-  return (
-    <div className="flex items-start p-2">
-      <h2 className="text-2xl font-extrabold">
-        {title}
-      </h2>
-    </div>
-  );
-}
